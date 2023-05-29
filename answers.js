e6caa7976ac9d16b440980d9175ebffd69e1380a
mapahama <nodrik00@abv.bg>
2023-05-29 17:26:47 +0200

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,10 +1,10 @@
 // answers, which aim to indicate  the identity of the personal assistant "Api"
 let intro = ["I am your digital assistant Api!", "Hi, I am Api!", "My name is Api",
- "I am your personal assistent Api!", "I am machine, named Api!"];
+ "I am your personal assistant Api!", "I am machine, named Api!"];
 
- let help =["how may I assistant you", "how I can halp you", "what can I do for you?"];
+ let help =["How may I assistant you", "How I can halp you", "What can I do for you?"];
 
- let thanks = ["most welcome", "I am glad to help you"];
+ let thanks = ["Most welcome", "I am glad to help you"];
 
 
 
