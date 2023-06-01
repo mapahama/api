0d6b75ae15915afaa40ccbc23a11e84ae71e1938
mapahama <nodrik00@abv.bg>
2023-06-01 14:23:05 +0200

    Update eventHandling2.js

--- eventHandling2.js
+++ eventHandling2.js
@@ -1,5 +1,4 @@
 
-var isSpeaking = synth.speaking;
 /*
 * Handling event click on maleBtn.
 */
@@ -61,6 +60,7 @@ mic.addEventListener('click', () => {
   messages_area.append(assistantSpeak(speech.text));
   synth.speak(speech);
   
+  var isSpeaking = synth.speaking;
   var counter = 0;
   //start recognition
   while(isSpeaking){
